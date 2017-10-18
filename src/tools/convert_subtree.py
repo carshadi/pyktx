9d1115b9c0346b907708d0d5b9c426e9a0dfb586
Konrad Rokicki <rokickik@janelia.hhmi.org>
2017-10-18 11:31:27 -0400

    eliminated specimen name from filenames

--- src/tools/convert_subtree.py
+++ src/tools/convert_subtree.py
@@ -11,8 +11,7 @@ import time
 
 from ktx.octree.ktx_from_rendered_tiff import RenderedMouseLightOctree
 
-
-def convert_subtree(            
+def convert_subtree(
             input_root_folder,
             output_root_folder,
             subtree_tip=[],
@@ -27,10 +26,9 @@ def convert_subtree(
     tip = "/".join([str(n) for n in subtree_tip])
     tip_folder = o.input_folder + "/" + tip
     print ( tip_folder )
-    specimen = os.path.split(input_root_folder)[1]
     for block in o.iter_blocks(folder=tip_folder, max_level=subtree_level_count-1):
         # print(block.octree_path)
-        file_name = 'block_' + specimen
+        file_name = 'block'
         if downsample_intensity:
             file_name += '_8'
         if downsample_xy:
@@ -58,17 +56,16 @@ def convert_subtree(
             f.flush()
             f.close()
             os.unlink(f.name)
-            
+
 
 def convert_subtree_simple(input_path, output_path, subtree_path, subtree_level_count):
     t0 = time.time()
     print( "converting subtree to ktx" )
-    specimen = os.path.split(input_path)[1]
     downsample_intensity = True
     downsample_xy = True
     convert_subtree(
             input_root_folder=input_path,
-            output_root_folder=output_path + '/' + specimen,
+            output_root_folder=output_path,
             subtree_tip=subtree_path,
             subtree_level_count=subtree_level_count,
             downsample_intensity=downsample_intensity, 
@@ -76,20 +73,20 @@ def convert_subtree_simple(input_path, output_path, subtree_path, subtree_level_
     t1 = time.time()
     print ("converting octree subtree to ktx format took %.3f seconds" % (t1 - t0))
 
+
 if __name__ == "__main__":
     input_path = sys.argv[1]
     output_path = sys.argv[2]
     # octree_path = [5,4,8,3,2,4]
     # octree_path = [6,2,7,3,1,8]
     # octree_path = [1,]
+
     if sys.argv[3] == '':
         subtree_path = []
     else:
         subtree_path = [int(x) for x in sys.argv[3].split('/')]
     level_count = int(sys.argv[4])
-    specimen = os.path.split(input_path)[1]
-    print(specimen)
-    #
+
     print(input_path)
     print(output_path)
     print(subtree_path)
@@ -99,3 +96,4 @@ if __name__ == "__main__":
         output_path=output_path,
         subtree_path=subtree_path,
         subtree_level_count=level_count,)
+
